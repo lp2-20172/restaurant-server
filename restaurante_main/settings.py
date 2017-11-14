2608461036edaf2c44dde738da74b7070b7761d5
Maridol <mariadqm@gmail.com>
2017-11-14 08:01:22 -0500

    imagen

--- restaurante_main/settings.py
+++ restaurante_main/settings.py
@@ -133,6 +133,9 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
