c71ba846dee591e0256ce9b6651dc8a933464696
WillianHuamanttupaMar <willian.hm7@gmail.com>
2017-11-14 08:10:01 -0500

    fikfff

--- restaurante_main/wsgi.py
+++ restaurante_main/wsgi.py
@@ -1,11 +1,4 @@
-"""
-WSGI config for restaurante_main project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +7,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restaurante_main.settings")
 
 application = get_wsgi_application()
+
